5f1b3f8606be0e4a6bb5d64c1d6f094a0837a6a3
jcc <370234915@qq.com>
2020-09-26 23:00:30 +0800

    update

---
+++ Plugin.php
@@ -0,0 +1,46 @@
+<?php namespace Jcc\Sidenav;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * sidenav Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'sidenav',
+            'description' => '扩展侧边拦插件',
+            'author'      => 'jcc',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+}
